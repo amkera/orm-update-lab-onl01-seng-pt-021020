d43401bfca235b7cfff5fd506a9f502d5c7a8b9f
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-14 21:28:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+  attr_accessor :name, :age 
+  attr_reader :id 
+  
+  def initialize(id=nil, name, age)
+    @id = id
+    @name = name
+    @age = age
+  end
 
 end

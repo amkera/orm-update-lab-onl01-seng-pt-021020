e296a73ce7a8a08baad5dd9f166cc974477af1b3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-14 21:55:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,40 +48,26 @@ class Student
     end
   end 
   
-  
-  
-  
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
-  
+
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
     grade = row[2]
     self.new(id, name, grade)
   end 
-  
-  def self.find_by_name
-    sql = <<-SQL 
-      SELECT * FROM students
-      WHERE name = ?
-    SQL
-    DB[:conn].execute(sql, name).map do |row|
-      new_from_db(row)
-    end.first
-  end 
-  
-  
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end 
-    
-  
-  
-    
-
+  end
 end
